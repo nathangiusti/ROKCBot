558cc289c98867c0e76aaa743f1009285e9af1c9
Nathan Giusti <ngiusti@dataxu.com>
2016-10-28 13:41:32 -0400

    Added some methods for incrementing maps Added filter support for usernames Added option parser Added normalization for crush/fan counts

--- CrushBotRawNumber.rb
+++ CrushBotRawNumber.rb
@@ -1,9 +1,51 @@
 require 'csv'
 require 'date'
+require 'optparse'
+require 'ostruct'
 
-poster_un = ARGV[2]
-start_date = Date.parse(ARGV[0])
-end_date = Date.parse(ARGV[1])
+def increment_map(map, value)
+  if map.key?(value)
+    map[value] = map[value]+1.0
+  else
+    map[value] = 1.0
+  end
+end
+
+def is_not_filtered?(val)
+  return (@@filter_array.nil? || @@filter_array.include?(val))
+end
+
+options = OpenStruct.new
+
+OptionParser.new do |opts|
+
+  opts.banner = "Usage: example.rb [options]"
+
+  opts.on("--start_date start_date") do |v|
+    options.start_date = v
+  end
+
+  opts.on("--end_date end_date") do |v|
+    options.end_date = v
+  end
+
+  opts.on("-f filter_list", "--filter_list filter_list") do |v|
+    options.filter_list = v
+  end
+
+  opts.on("-u user_name", "--user_name user_name") do |v|
+    options.user_name = v
+  end
+
+  opts.on("-n", "--normalize") do |v|
+    options.normalize = true
+  end
+
+end.parse!
+
+start_date = Date.parse(options.start_date)
+end_date = Date.parse(options.end_date)
+@@filter_array = nil
 ARCHIVE_DIR = "archive"
 
 #Generate a list of dates
@@ -13,63 +55,83 @@ for next_date in (start_date..end_date)
 end
 
 
+unless options.filter_list.nil?
+  @@filter_array = []
+  filter_file = File.open(options.filter_list)
+  filter_file.each do |line|
+    @@filter_array.push(line.strip)
+  end
+end
+
+comment_count = 0
+fan_comment_count = 0
+crush_comment_count = 0
+bulk_fan_map = {};
+bulk_crush_map = {};
+crush_map = {}
+fan_map = {}
+
 for date in date_list
   reply_csv = CSV.open("#{ARCHIVE_DIR}/Reply_#{date}.txt")
   comment_csv = File.open("#{ARCHIVE_DIR}/Comment_#{date}.txt")
 
   #Get number of comments by user
-  count = 0
   comment_csv.each do |line|
-    if line.strip == poster_un
-      count += 1
+    if line.strip == options.user_name
+      comment_count += 1
     end
   end
 
-  crush_map = {}
-  fan_map = {}
-
-  fan_count = 0;
-  crush_count = 0;
 
   reply_csv.each do |row|
-    unless row[0] == poster_un || row[1] == poster_un
-      next
-    else
-      if row[0] == poster_un
-        fan_count += 1
-        if fan_map.key?(row[1])
-          fan_map[row[1]] = fan_map[row[1]]+1
-        else
-          fan_map[row[1]] = 1
-        end
-      else
-        crush_count += 1
-        if crush_map.key?(row[0])
-          crush_map[row[0]] = crush_map[row[0]]+1
-        else
-          crush_map[row[0]] = 1
-        end
-      end
+
+    if is_not_filtered?(row[1]) && is_not_filtered?(row[0])
+      increment_map(bulk_fan_map, row[1])
+      increment_map(bulk_crush_map, row[0])
     end
+
+    if row[0] == options.user_name && is_not_filtered?(row[1])
+      increment_map(fan_map, row[1])
+      fan_comment_count += 1
+    elsif row[1] == options.user_name && is_not_filtered?(row[0])
+      increment_map(crush_map, row[0])
+      crush_comment_count += 1
+    end
+
   end
 end
 
-puts "#{poster_un}: #{count}"
-puts "\n\\----------------------------------------------------\n"
-puts "#{fan_map.keys.length} fans in #{fan_count} comments"
-
+if options.normalize
+  fan_map.each do |key, value|
+    fan_map[key] = fan_map[key]/bulk_fan_map[key]
+  end
+  crush_map.each do |key, value|
+    crush_map[key] = crush_map[key]/bulk_crush_map[key]
+  end
+end
 max_fan_arr = fan_map.sort_by { |k, v| v }
 max_fan_arr.reverse!
+max_crush_arr = crush_map.sort_by { |k, v| v }
+max_crush_arr.reverse!
+
+
+
+
+
+puts "#{options.user_name}: #{comment_count}"
+puts "\n\\----------------------------------------------------\n"
+puts "#{fan_map.keys.length} fans in #{fan_comment_count} comments"
+
+
 
 for i in 0..4
   puts "\n#{max_fan_arr[i][0].ljust(25)}: #{max_fan_arr[i][1]}"
 end
 
 puts "\n\\----------------------------------------------------\n"
-puts "#{crush_map.keys.length} fans in #{crush_count} comments"
+puts "#{crush_map.keys.length} crushes in #{crush_comment_count} comments"
 
-max_crush_arr = crush_map.sort_by { |k, v| v }
-max_crush_arr.reverse!
+
 
 for i in 0..4
   puts "\n#{max_crush_arr[i][0].ljust(25)}: #{max_crush_arr[i][1]}"
